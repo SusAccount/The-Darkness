24c8865b6faa11c60916b394a0cf19bd65aa9ae1
SusAccount <110735685+SusAccount@users.noreply.github.com>
2022-10-31 20:05:48 -0300

    Add files via upload

---
+++ assets/preload/data/darkness/script.lua
@@ -0,0 +1,160 @@
+	function onStepHit()
+		if curStep == 1 then
+			noteTweenAlpha("Note2", 2, 0, 0.5, ".cubeInOut") --up opponent
+			noteTweenAlpha("Note0", 0, 0, 0.5, ".cubeInOut") --left opponent
+			noteTweenAlpha("Note1", 1, 0, 0.5, ".cubeInOut") --down opponent
+			noteTweenAlpha("Note3", 3, 0, 0.5, ".cubeInOut") --right opponent
+		end
+		if curStep == 1 then
+			noteTweenAlpha("Note4", 4, 0, 0.5, ".cubeInOut") --left bf
+			noteTweenAlpha("Note5", 5, 0, 0.5, ".cubeInOut") --down bf
+			noteTweenAlpha("Note6", 6, 0, 0.5, ".cubeInOut") --up bf
+			noteTweenAlpha("Note7", 7, 0, 0.5, ".cubeInOut") --right bf
+		end
+		-- AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+		if curStep == 40 then
+			noteTweenAlpha("Note5", 5, 0.5, 0.5, ".cubeInOut") --down bf
+		end
+		if curStep == 42 then
+			noteTweenAlpha("Note7", 7, 0.5, 0.5, ".cubeInOut") --right bf
+		end
+		if curStep == 44 then
+			noteTweenAlpha("Note6", 6, 0.5, 0.5, ".cubeInOut") --up bf
+			noteTweenAlpha("Note5", 5, 0, 0.5, ".cubeInOut") --down bf
+		end
+		if curStep == 52 then
+			noteTweenAlpha("Note4", 4, 0.5, 0.5, ".cubeInOut") --left bf
+			noteTweenAlpha("Note6", 6, 0, 0.5, ".cubeInOut") --up bf
+		end
+		if curStep == 58 then
+			noteTweenAlpha("Note5", 5, 0.5, 0.5, ".cubeInOut") --down bf
+			noteTweenAlpha("Note7", 7, 0, 0.5, ".cubeInOut") --right bf
+		end
+		if curStep == 60 then
+			noteTweenAlpha("Note6", 6, 0.5, 0.5, ".cubeInOut") --up bf
+		end
+		if curStep == 70 then
+			noteTweenAlpha("Note4", 4, 0, 0.5, ".cubeInOut") --left bf
+			noteTweenAlpha("Note5", 5, 0, 0.5, ".cubeInOut") --down bf
+			noteTweenAlpha("Note6", 6, 0, 0.5, ".cubeInOut") --up bf
+			noteTweenAlpha("Note7", 7, 0, 0.5, ".cubeInOut") --right bf
+		end
+		if curStep == 84 then
+			noteTweenAlpha("Note7", 7, 0.5, 0.5, ".cubeInOut") --right bf
+		end
+		if curStep == 86 then
+			noteTweenAlpha("Note4", 4, 0.5, 0.5, ".cubeInOut") --left bf
+		end
+		if curStep == 88 then
+			noteTweenAlpha("Note6", 6, 0.5, 0.5, ".cubeInOut") --up bf
+		end
+		if curStep == 96 then
+			noteTweenAlpha("Note6", 6, 0, 0.5, ".cubeInOut") --up bf
+			noteTweenAlpha("Note5", 5, 0.5, 0.5, ".cubeInOut") --down bf
+        end
+		if curStep == 98 then
+			noteTweenAlpha("Note6", 6, 0.5, 0.5, ".cubeInOut") --up bf
+		end
+		if curStep == 160 then
+			noteTweenAlpha("Note4", 4, 0.1, 0.5, ".cubeInOut") --left bf
+			noteTweenAlpha("Note6", 6, 0.1, 0.5, ".cubeInOut") --up bf
+			noteTweenAlpha("Note7", 7, 0, 0.5, ".cubeInOut") --right bf
+		end
+		if curStep == 168 then
+			noteTweenAlpha("Note5", 5, 0.1, 0.5, ".cubeInOut") --down bf
+		end
+		if curStep == 190 then
+			noteTweenAlpha("Note4", 4, 0, 0.5, ".cubeInOut") --left bf
+			noteTweenAlpha("Note5", 5, 0, 0.5, ".cubeInOut") --down bf
+			noteTweenAlpha("Note6", 6, 0, 0.5, ".cubeInOut") --up bf
+			noteTweenAlpha("Note7", 7, 0, 0.5, ".cubeInOut") --right bf
+		end
+		if curStep == 204 then
+			noteTweenAlpha("Note4", 4, 1, 0.5, ".cubeInOut") --left bf
+			noteTweenAlpha("Note5", 5, 1, 0.5, ".cubeInOut") --down bf
+			noteTweenAlpha("Note6", 6, 1, 0.5, ".cubeInOut") --up bf
+			noteTweenAlpha("Note7", 7, 1, 0.5, ".cubeInOut") --right bf
+		end
+		if curStep == 224 then
+			noteTweenAlpha("Note4", 4, 0, 0.5, ".cubeInOut") --left bf
+			noteTweenAlpha("Note5", 5, 0, 0.5, ".cubeInOut") --down bf
+			noteTweenAlpha("Note6", 6, 0, 0.5, ".cubeInOut") --up bf
+			noteTweenAlpha("Note7", 7, 0, 0.5, ".cubeInOut") --right bf
+		end
+		if curStep == 236 then
+			noteTweenAlpha("Note4", 4, 1, 0.5, ".cubeInOut") --left bf
+			noteTweenAlpha("Note5", 5, 1, 0.5, ".cubeInOut") --down bf
+			noteTweenAlpha("Note6", 6, 1, 0.5, ".cubeInOut") --up bf
+			noteTweenAlpha("Note7", 7, 1, 0.5, ".cubeInOut") --right bf
+		end
+		if curStep == 256 then
+			noteTweenAlpha("Note4", 4, 0, 0.5, ".cubeInOut") --left bf
+			noteTweenAlpha("Note5", 5, 0, 0.5, ".cubeInOut") --down bf
+			noteTweenAlpha("Note6", 6, 0, 0.5, ".cubeInOut") --up bf
+			noteTweenAlpha("Note7", 7, 0, 0.5, ".cubeInOut") --right bf
+		end
+		if curStep == 276 then
+			noteTweenAlpha("Note4", 4, 1, 0.5, ".cubeInOut") --left bf
+			noteTweenAlpha("Note5", 5, 1, 0.5, ".cubeInOut") --down bf
+			noteTweenAlpha("Note6", 6, 1, 0.5, ".cubeInOut") --up bf
+			noteTweenAlpha("Note7", 7, 1, 0.5, ".cubeInOut") --right bf
+		end
+		if curStep == 312 then
+			noteTweenAlpha("Note4", 4, 0, 0.5, ".cubeInOut") --left bf
+			noteTweenAlpha("Note5", 5, 0, 0.5, ".cubeInOut") --down bf
+			noteTweenAlpha("Note6", 6, 0, 0.5, ".cubeInOut") --up bf
+			noteTweenAlpha("Note7", 7, 0, 0.5, ".cubeInOut") --right bf
+		end
+		if curStep == 332 then
+			noteTweenAlpha("Note4", 4, 1, 0.5, ".cubeInOut") --left bf
+			noteTweenAlpha("Note5", 5, 1, 0.5, ".cubeInOut") --down bf
+			noteTweenAlpha("Note6", 6, 1, 0.5, ".cubeInOut") --up bf
+			noteTweenAlpha("Note7", 7, 1, 0.5, ".cubeInOut") --right bf
+		end
+		if curStep == 352 then
+			noteTweenAlpha("Note4", 4, 0, 0.5, ".cubeInOut") --left bf
+			noteTweenAlpha("Note5", 5, 0, 0.5, ".cubeInOut") --down bf
+			noteTweenAlpha("Note6", 6, 0, 0.5, ".cubeInOut") --up bf
+			noteTweenAlpha("Note7", 7, 0, 0.5, ".cubeInOut") --right bf
+		end
+		if curStep == 364 then
+			noteTweenAlpha("Note4", 4, 1, 0.5, ".cubeInOut") --left bf
+			noteTweenAlpha("Note5", 5, 1, 0.5, ".cubeInOut") --down bf
+			noteTweenAlpha("Note6", 6, 1, 0.5, ".cubeInOut") --up bf
+			noteTweenAlpha("Note7", 7, 1, 0.5, ".cubeInOut") --right bf
+		end
+		if curStep == 400 then
+			noteTweenAlpha("Note4", 4, 0, 0.5, ".cubeInOut") --left bf
+			noteTweenAlpha("Note5", 5, 0, 0.5, ".cubeInOut") --down bf
+			noteTweenAlpha("Note6", 6, 0, 0.5, ".cubeInOut") --up bf
+			noteTweenAlpha("Note7", 7, 0, 0.5, ".cubeInOut") --right bf
+		end
+		if curStep == 400 then
+			noteTweenAlpha("Note3", 3, 1, 0.5, ".cubeInOut") --right opponent
+		end
+		if curStep == 432 then
+			noteTweenAlpha("Note3", 3, 0, 0.5, ".cubeInOut") --right opponent
+		end
+		if curStep == 461 then
+			noteTweenAlpha("Note4", 4, 1, 0.5, ".cubeInOut") --left bf
+			noteTweenAlpha("Note5", 5, 1, 0.5, ".cubeInOut") --down bf
+			noteTweenAlpha("Note6", 6, 1, 0.5, ".cubeInOut") --up bf
+			noteTweenAlpha("Note7", 7, 1, 0.5, ".cubeInOut") --right bf
+		end
+		if curStep == 480 then
+			noteTweenAlpha("Note2", 2, 1, 0.5, ".cubeInOut") --up opponent
+			noteTweenAlpha("Note0", 0, 1, 0.5, ".cubeInOut") --left opponent
+			noteTweenAlpha("Note1", 1, 1, 0.5, ".cubeInOut") --down opponent
+			noteTweenAlpha("Note3", 3, 1, 0.5, ".cubeInOut") --right opponent
+		end
+		if curStep == 550 then
+		noteTweenAlpha("Note4", 4, 0, 0.5, ".cubeInOut") --left bf
+		noteTweenAlpha("Note5", 5, 0, 0.5, ".cubeInOut") --down bf
+		noteTweenAlpha("Note6", 6, 0, 0.5, ".cubeInOut") --up bf
+		noteTweenAlpha("Note7", 7, 0, 0.5, ".cubeInOut") --right bf
+		noteTweenAlpha("Note2", 2, 0, 0.5, ".cubeInOut") --up opponent
+		noteTweenAlpha("Note0", 0, 0, 0.5, ".cubeInOut") --left opponent
+		noteTweenAlpha("Note1", 1, 0, 0.5, ".cubeInOut") --down opponent
+		noteTweenAlpha("Note3", 3, 0, 0.5, ".cubeInOut") --right opponent
+		end
+	end
